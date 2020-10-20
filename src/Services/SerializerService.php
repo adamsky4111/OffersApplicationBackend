819c3dd2e472e8286a89fdd10baf4422a923792c
adamsky4111 <adamdz411@gmail.com>
2020-10-21 01:18:03 +0200

    add comments

--- src/Services/SerializerService.php
+++ src/Services/SerializerService.php
@@ -13,6 +13,12 @@ use App\Utils\EntitySerializers\OfferSerializer;
 use App\Utils\EntitySerializers\PropositionSerializer;
 use Symfony\Component\Serializer\SerializerInterface;
 
+/**
+ * Class SerializerService
+ * This is class for handle serialization of entities in application.
+ * It uses serializers in App\Utils\EntitySerializers for specific serialization.
+ * It's based on Strategy pattern to set up serialization strategy based on entry entity.
+ */
 class SerializerService implements SerializerServiceInterface
 {
     private SerializerInterface $serializer;
@@ -29,6 +35,7 @@ class SerializerService implements SerializerServiceInterface
         ];
     }
 
+    // method for serialize entry entities in array.=
     public function serializeList(array $data)
     {
         $entityName = get_class($data[0]);
@@ -37,6 +44,7 @@ class SerializerService implements SerializerServiceInterface
         return $this->serializationStrategy->entitiesToJson($data);
     }
 
+    // method for serialize single entity
     public function serializeObject(BaseEntity $data)
     {
         $entityName = get_class($data);
@@ -45,6 +53,7 @@ class SerializerService implements SerializerServiceInterface
         return $this->serializationStrategy->entityToJson($data);
     }
 
+    // method to deserialize entity
     public function deserializeObject(string $data, $entityName)
     {
         $this->setSerializationStrategyByEntityName($entityName);
@@ -52,6 +61,7 @@ class SerializerService implements SerializerServiceInterface
         return $this->serializationStrategy->jsonToEntity($data);
     }
 
+    // private function to set strategy based on className property passed by parameter
     private function setSerializationStrategyByEntityName($className)
     {
         $this->serializationStrategy = $this->serializationStrategies[$className];
--- src/Utils/EntitySerializers/AbstractEntitySerializer.php
+++ src/Utils/EntitySerializers/AbstractEntitySerializer.php
@@ -6,6 +6,10 @@ use App\Entity\BaseEntity;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
+/**
+ * Class AbstractEntitySerializer
+ * This is abstract class for generic use cases.
+ */
 abstract class AbstractEntitySerializer implements EntitySerializerInterface
 {
     protected SerializerInterface $serializer;
@@ -17,15 +21,21 @@ abstract class AbstractEntitySerializer implements EntitySerializerInterface
     {
         $this->serializer = $serializer;
         $this->entityName = $entityName;
+
+        // If serialization prefix is passed by constructor
         if (null !== $serializationGroupsPrefix) {
             $this->serializationGroupsPrefix = $serializationGroupsPrefix;
         } else {
-            //Create prefix for serialization groups, for instance, for Category class it generate 'category'
+            // if serialization prefix isn't passed by construct, it's necessary to
+            // create prefix for serialization groups, for instance:
+            // we have entity Product in App\Entity so it take Product without App\Entity namespace
+            // then it format string to lower. We do not need to pass prefix by construct.
             $entityBasename = basename(str_replace('\\', '/', $entityName));
             $this->serializationGroupsPrefix = strtolower($entityBasename);
         }
     }
 
+    // generic many entities to json, serialization groups is based on parameters passed by construct
     public function entitiesToJson(array $data)
     {
         if (!($data[0] instanceof $this->entityName)) {
@@ -39,6 +49,7 @@ abstract class AbstractEntitySerializer implements EntitySerializerInterface
         );
     }
 
+    // generic one entity to json, serialization groups is based on parameters passed by construct
     public function entityToJson(BaseEntity $data)
     {
         if (!($data instanceof $this->entityName)) {
@@ -52,6 +63,7 @@ abstract class AbstractEntitySerializer implements EntitySerializerInterface
         );
     }
 
+    // generic json to entity
     public function jsonToEntity(string $data)
     {
         return $this->serializer->deserialize(
