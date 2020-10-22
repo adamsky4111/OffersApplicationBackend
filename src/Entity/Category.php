e9c61ae5a89c4d3bba74d447ac1e1351454b7864
adamsky4111 <adamdz411@gmail.com>
2020-10-22 05:58:55 +0200

    change repository namespace in Category.php

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Repository\CategoryRepository;
+use App\Repository\Doctrine\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
