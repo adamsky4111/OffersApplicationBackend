92d8f403cdfda3b518d22fe0a2894645ed31229f
adamsky4111 <adamdz411@gmail.com>
2020-10-22 21:07:22 +0200

    add CategoryCollector.php

---
+++ src/Utils/EntityCollectors/CategoryCollector/CategoryCollector.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Utils\EntityCollectors\CategoryCollector;
+
+use App\Repository\Current\CategoryRepository\CategoryRepositoryInterface;
+
+class CategoryCollector implements CategoryCollectorInterface
+{
+    private CategoryRepositoryInterface $repository;
+
+    public function __construct(CategoryRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function getOne($id)
+    {
+        return $this->repository->findOne($id);
+    }
+
+    public function getAllActive()
+    {
+        return $this->repository->findAllActive();
+    }
+}
