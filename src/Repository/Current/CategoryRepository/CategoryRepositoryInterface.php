a7c2e7dfd6a5bb9cf986a296e58305e38df6ab15
adamsky4111 <adamdz411@gmail.com>
2020-10-22 11:59:00 +0200

    add save and remove to PropositionRepositoryInterface.php and CategoryRepositoryInterface.php

--- src/Repository/Current/CategoryRepository/CategoryRepositoryInterface.php
+++ src/Repository/Current/CategoryRepository/CategoryRepositoryInterface.php
@@ -2,8 +2,14 @@
 
 namespace App\Repository\Current\CategoryRepository;
 
+use App\Entity\BaseEntity;
+
 interface CategoryRepositoryInterface
 {
+    public function save(BaseEntity $entity);
+
+    public function remove(BaseEntity $entity);
+
     public function getOne($id);
 
     public function getAllActive();
--- src/Repository/Current/PropositionRepository/PropositionRepositoryInterface.php
+++ src/Repository/Current/PropositionRepository/PropositionRepositoryInterface.php
@@ -2,8 +2,14 @@
 
 namespace App\Repository\Current\PropositionRepository;
 
+use App\Entity\BaseEntity;
+
 interface PropositionRepositoryInterface
 {
+    public function save(BaseEntity $entity);
+
+    public function remove(BaseEntity $entity);
+
     public function getOne($id);
 
     public function getAllActive();
