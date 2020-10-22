e4f31110fd859d3b351af7338f4792a234ec66f5
adamsky4111 <adamdz411@gmail.com>
2020-10-22 21:07:15 +0200

    add CategoryCollectorInterface.php in new location

---
+++ src/Utils/EntityCollectors/CategoryCollector/CategoryCollectorInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Utils\EntityCollectors\CategoryCollector;
+
+use App\Repository\Current\CategoryRepository\CategoryRepositoryInterface;
+
+interface CategoryCollectorInterface
+{
+    public function __construct(CategoryRepositoryInterface $repository);
+
+    public function getOne($id);
+
+    public function getAllActive();
+}
