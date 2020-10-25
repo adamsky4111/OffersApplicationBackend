90114e902be039e74fd65125b8d4ecc18cae59f5
adamsky4111 <adamdz411@gmail.com>
2020-10-25 04:39:56 +0100

    add CategoryManager.php

---
+++ src/Utils/EntityManagers/CategoryManager/CategoryManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Utils\EntityManagers\CategoryManager;
+
+use App\Entity\Category;
+use App\Utils\EntityManagers\AbstractSpecificEntityManager;
+
+/**
+ * @property Category $entity
+ */
+class CategoryManager extends AbstractSpecificEntityManager implements CategoryManagerInterface
+{
+}
