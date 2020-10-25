7a02b7ae525f096a4402df8def1c513fc036c059
adamsky4111 <adamdz411@gmail.com>
2020-10-25 07:43:02 +0100

    fix CategorySorter

--- src/Utils/EntitySorter/CategorySorter/CategorySorter.php
+++ src/Utils/EntitySorter/CategorySorter/CategorySorter.php
@@ -3,25 +3,27 @@
 namespace App\Utils\EntitySorter\CategorySorter;
 
 use App\Utils\EntitySorter\AbstractEntitySorter;
-use App\Utils\EntitySorter\Interfaces\CategorySorterInterface;
 
 class CategorySorter extends AbstractEntitySorter implements CategorySorterInterface
 {
-    public function getSortedByCreatedAt(array $data, bool $desc = false)
+    public function sortByCreatedAt(array $data, bool $desc = false)
     {
-        return $this->setDataAndOneSort($data, 'createdAt', $desc)
-            ->getResult();
+        $this->setDataAndOneSort($data, 'createdAt', $desc);
+
+        return $this;
     }
 
-    public function getSortedByUpdatedAt(array $data, bool $desc = false)
+    public function sortByUpdatedAt(array $data, bool $desc = false)
     {
-        return $this->setDataAndOneSort($data, 'updatedAt', $desc)
-            ->getResult();
+        $this->setDataAndOneSort($data, 'updatedAt', $desc);
+
+        return $this;
     }
 
-    public function getSortedByName(array $data, bool $desc = false)
+    public function sortByName(array $data, bool $desc = false)
     {
-        return $this->setDataAndOneSort($data, 'name', $desc)
-            ->getResult();
+        $this->setDataAndOneSort($data, 'name', $desc);
+
+        return $this;
     }
 }
